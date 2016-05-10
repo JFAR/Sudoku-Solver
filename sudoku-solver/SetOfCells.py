77e89384c16c148d54281b3cfb8c9a9103296b26
Jafar Miles <jafarmiles@gmail.com>
2016-05-10 23:15:59 +0100

    Adding ability for SetOfCells to reduce cells which belong to it

--- sudoku-solver/SetOfCells.py
+++ sudoku-solver/SetOfCells.py
@@ -4,3 +4,14 @@ class SetOfCells:
 
     def returnCells(self):
         return self.cells
+
+    def reduceCells(self):
+        determinedValues = {cell.getOptions()[0] for cell in self.cells if cell.isSet()}
+
+        for cell in self.cells:
+            if cell.isSet() is False:
+                cell.removeOptions(determinedValues)
+
+                if cell.isSet():
+                    self.reduceCells()
+                    break
--- sudoku-solver/test/test_setOfCells.py
+++ sudoku-solver/test/test_setOfCells.py
@@ -12,3 +12,17 @@ class SetOfCellsTest(unittest.TestCase):
         setOfCells = SetOfCells.SetOfCells(cells)
 
         self.assertSetEqual(setOfCells.returnCells(), set(cells))
+
+    def test_reduceCells(self):
+        cells = []
+        for i in range(8):
+            cells.append(SudokuCell.Cell(0, i, [i+1]))
+
+        cells.append(SudokuCell.Cell(0, 8))
+
+        expectedCells = set([x + 1 for x in range(9)])
+
+        setOfCells = SetOfCells.SetOfCells(cells)
+        setOfCells.reduceCells()
+
+        self.assertSetEqual({cell.getOptions()[0] for cell in setOfCells.returnCells()}, expectedCells)
