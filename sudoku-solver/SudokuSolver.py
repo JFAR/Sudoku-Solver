3364ad70e255f83c63760a4bafeb1c5116ab435f
Jafar Miles <jafarmiles@gmail.com>
2016-05-24 08:18:35 +0100

    Adding Extreme sudoku test case and fixed to pass

--- sudoku-solver/SudokuSolver.py
+++ sudoku-solver/SudokuSolver.py
@@ -4,23 +4,40 @@ import copy
 
 def solver(inputBoard):
     sudokuBoard = solveIteration(sudokuSolver(SudokuBoard(inputBoard)))
-    return sudokuBoard.returnBoard()
+    outputBoard = sudokuBoard.returnBoard()
+    return outputBoard
 
 
 def sudokuSolver(sudokuBoard):
     if sudokuBoard is False:
         return False
-    elif sudokuBoard.isSolved():
+    elif sudokuBoard.isLegal() is False:
+        return False
+    else:
+        solveIteration(sudokuBoard)
+
+    if sudokuBoard.isSolved():
         return sudokuBoard
     else:
+        solveIteration(sudokuBoard)
         cell = sudokuBoard.findFirstUnSetCell()
         for option in cell.getOptions():
-            cell.set(option)
-            newSudokuBoard = solveIteration(copy.deepcopy(sudokuBoard))
-            if newSudokuBoard.isLegal():
-                return sudokuSolver(newSudokuBoard)
+            potentialSolution = copy.deepcopy(sudokuBoard)
+            potentialCell = potentialSolution.getCell(cell.getRowId(), cell.getColumnId())
+            potentialCell.set(option)
+
+            newSudokuBoard = sudokuSolver(solveIteration(potentialSolution))
+
+            if newSudokuBoard is not False:
+                return newSudokuBoard
 
         return False
+
+
+def prettyPrint(board):
+    for row in board:
+        print(row)
+    print()
 
 
 def solveIteration(sudokuBoard):
--- sudoku-solver/test/test_sudokuSolver.py
+++ sudoku-solver/test/test_sudokuSolver.py
@@ -74,6 +74,34 @@ class SudokuTest(unittest.TestCase):
 
         self.assertEqual(SudokuSolver.solver(mediumSudoku), solvedSudoku)
 
+    def test_solvesExtremeSudoku(self):
+        extremeSudoku = [[9, 0, 0,  0, 8, 5,  0, 0, 0],
+                         [0, 6, 0,  1, 0, 0,  0, 0, 0],
+                         [8, 0, 4,  0, 0, 0,  2, 1, 0],
+
+                         [0, 0, 8,  6, 0, 0,  4, 9, 0],
+                         [0, 0, 0,  5, 9, 7,  0, 0, 0],
+                         [0, 9, 7,  0, 0, 4,  5, 0, 0],
+
+                         [0, 1, 5,  0, 0, 0,  9, 0, 2],
+                         [0, 0, 0,  0, 0, 1,  0, 7, 0],
+                         [0, 0, 0,  3, 5, 0,  0, 0, 6]]
+
+        solvedSudoku = [[9, 7, 1,  2, 8, 5,  6, 3, 4],
+                        [2, 6, 3,  1, 4, 9,  7, 5, 8],
+                        [8, 5, 4,  7, 6, 3,  2, 1, 9],
+
+                        [5, 3, 8,  6, 1, 2,  4, 9, 7],
+                        [1, 4, 2,  5, 9, 7,  8, 6, 3],
+                        [6, 9, 7,  8, 3, 4,  5, 2, 1],
+
+                        [3, 1, 5,  4, 7, 6,  9, 8, 2],
+                        [4, 8, 6,  9, 2, 1,  3, 7, 5],
+                        [7, 2, 9,  3, 5, 8,  1, 4, 6]]
+
+        self.assertEqual(SudokuSolver.solver(solvedSudoku), solvedSudoku)
+        self.assertEqual(SudokuSolver.solver(extremeSudoku), solvedSudoku)
+
     def test_solvesEasySudoku(self):
         inputBoard = copy.deepcopy(self.outputBoard)
         inputBoard[8][8] = 0
